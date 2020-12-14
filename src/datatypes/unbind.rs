f3ddb2b3ea36f4bc790eaf40d29836d251c76c02
Iain H <iain@spiralpoint.org>
2020-12-13 22:11:01 -0500

    Unbind: Add missed derives and a short description from the SMPP spec.

--- src/datatypes/unbind.rs
+++ src/datatypes/unbind.rs
@@ -1,5 +1,12 @@
 use crate::{CommandId, CommandStatus};
 
+/// The purpose of the SMPP unbind operation is to deregister an instance of an ESME from the SMSC
+/// and inform the SMSC that the ESME no longer wishes to use this network connection for the
+/// submission or delivery of messages.
+///
+/// Thus, the unbind operation may be viewed as a form of SMSC logoff request to close the current
+/// SMPP session.
+#[derive(Clone, Debug, PartialEq)]
 pub struct Unbind {
     // pub command_length: u32,
     // pub command_id: CommandId::Unbind,
@@ -7,6 +14,7 @@ pub struct Unbind {
     pub sequence_number: u32,
 }
 
+#[derive(Clone, Debug, PartialEq)]
 pub struct UnbindResponse {
     // pub command_length: u32,
     // pub command_id: CommandId::UnbindResponse,
